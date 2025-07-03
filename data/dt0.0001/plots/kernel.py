4d3fde6c951f936752b48afc2ea04f0c1e52a722
Ruojing Peng <rpeng@holylogin06.rc.fas.harvard.edu>
2025-07-03 10:40:29 -0400

    push

---
+++ data/dt0.0001/plots/kernel.py
@@ -0,0 +1,120 @@
+import numpy as np
+import scipy.linalg
+import h5py
+from GQME_discretization_error.ttm import * 
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=10000,suppress=True,linewidth=10000)
+plt.rcParams.update({'font.size':16})
+plt.rcParams.update({'figure.figsize':(6.4,4.8)})
+
+dt_min = .0001
+itv = int(dt_min/.0001+1e-6)
+M = int(3/dt_min+1e-6) 
+rhos_ex = [None] * 4
+dir_ = '../heom_run/'
+for i in range(4):
+    f = h5py.File(dir_+f'long_path{i}.hdf5', 'r')
+    rhos_ex[i] = process_trajectory(f['rho'][:][::itv][:M])
+    f.close()
+Uex = rho2U(rhos_ex)
+
+colors = 'blue','orange','green','red','cyan'
+fnames = (1,0,'real'),(1,0,'imag'),(1,1,'real'),(1,2,'real'),(None,None,'err')
+ylabs = 'Re'+r'$\mathcal{K}_{01,00}$',\
+        'Im'+r'$\mathcal{K}_{01,00}$',\
+        'Re'+r'$\mathcal{K}_{01,01}$',\
+        'Re'+r'$\mathcal{K}_{01,10}$',\
+        r'$\log_{10}||\Delta\mathcal{K}||$',
+figs = [plt.subplots(nrows=1,ncols=1) for _ in range(5)]
+
+def plot(x,K,ls,c,l=None,mk=None,mksz=None,itv=1,kappa=None):
+    for (ix1,ix2,typ),(fig,ax) in zip(fnames,figs):
+        mksz = 10 
+        if typ=='real':
+            y = K[:,ix1,ix2].real 
+        elif typ=='imag':
+            y = K[:,ix1,ix2].imag
+        else:
+            if kappa is None:
+                continue
+            dK = K+kappa[:K.shape[0]]
+            y = np.array([np.linalg.norm(dKi) for dKi in dK])
+            y = np.log10(y)
+            mksz = 5
+        ax.plot(x[::itv],y[::itv],linestyle=ls,color=c,marker=mk,markersize=mksz,label=l)
+
+# check kernel
+kappa = np.load(dir_+f'kappa_{dt_min}_4th.npy')
+dddU0 = np.load(dir_+f'dddU0_{dt_min}_4th.npy')
+F = np.load(dir_+f'F_{dt_min}_4th.npy')
+#F = np.load(dir_+'B2_0.0005_4th.npy')
+x = np.arange(M)*dt_min
+print(x[-1])
+plot(x,-kappa[:M],'-','k',l='exact')
+
+eps = 0.0
+delta = -1.0
+H = eps*sz + delta*sx
+Ls = H2L(H)
+
+rho0 = np.array([1,0,0,0])
+def TTMK1(T,dt):
+    K = T[1:].copy()
+    K[0] -= np.eye(4)-1j*dt*Ls
+    K /= dt**2
+    K[0] *= 2
+    K[0] += np.dot(Ls,Ls)
+    return K
+for dt,color in zip((0.01,0.05,0.1),colors):
+    itv = int(dt/dt_min+1e-6)
+    print('dt=',dt,itv)
+    U = Uex[::itv]
+    T,_ = U2T(U)
+
+    # TTM w/ F correction
+    K = TTMK1(T,dt)
+    K[0] -= dt*dddU0/3
+    K[1:] -= dt*F[::itv][1:K.shape[0]]/2
+    itv_ = 3 if dt<0.02 else 1
+    x = np.arange(K.shape[0])*dt
+    l = r'$\Omega\Delta t=$'+str(dt)
+    plot(x,K,'-',color,l,mk='o',itv=itv_,kappa=kappa[::itv])
+
+    # TTM
+    K = TTMK1(T,dt)
+    itv_ = 3 if dt<0.02 else 1
+    x = np.arange(K.shape[0])*dt
+    plot(x,K,'--',color,mk='v',itv=itv_,kappa=kappa[::itv])
+
+    # midpoint
+    G1 = scipy.linalg.expm(-1j*dt*Ls)
+    Gh_inv = scipy.linalg.expm(1j*dt*Ls/2)
+
+    T = U.copy()
+    stop = U.shape[0]
+    T[0] = 0
+    T[1] = 2*U[1]-G1
+    for N in range(2,stop):
+        for m in range(1,N):
+            T[N] -= np.dot(T[m],U[N-m])
+        T[N] *= 2
+
+    K = -T.copy()
+    K[1] += G1
+    for i in range(1,stop):
+        K[i] = np.dot(Gh_inv,K[i])/dt**2
+    K = np.array([(K[i]+K[i+1])/2 for i in range(1,stop-1)])
+    x = np.arange(1,K.shape[0]+1)*dt
+    plot(x,-K,':',color,mk='+',itv=itv_,kappa=kappa[::itv])
+
+for (ix1,ix2,typ),(fig,ax),ylab in zip(fnames,figs,ylabs):
+    ax.set_xlabel(r'$\Omega t$')
+    ax.set_ylabel(ylab)
+    #ax.set_xlim(0,0.5)
+    ax.legend()
+    fig.subplots_adjust(left=0.17, bottom=0.15, right=0.95, top=0.98)
+    if typ=='err':
+        fig.savefig(f'K_{typ}_{dt_min}.png', dpi=250)
+    else:
+        fig.savefig(f'K{ix1}{ix2}_{typ}_{dt_min}.png', dpi=250)
+    plt.close(fig)
---
+++ data/dt0.0005/plots/drho.py
@@ -0,0 +1,103 @@
+import numpy as np
+import scipy.linalg
+import h5py
+from GQME_discretization_error.ttm import * 
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=10000,suppress=True,linewidth=10000)
+plt.rcParams.update({'font.size':16})
+plt.rcParams.update({'figure.figsize':(6.4,4.8)})
+
+dir_ = '../heom_run/'
+f = h5py.File(dir_+f'long_path0.hdf5', 'r')
+rho_ref = process_trajectory(f['rho'][:])
+f.close()
+
+kappa = np.load(dir_+'kappa_0.0005_4th.npy')
+dddU0 = np.load(dir_+'dddU0_0.0005_4th.npy')
+F = np.load(dir_+'F_0.0005_4th.npy')
+M = len(kappa) 
+rhos_ex = [None] * 4
+for i in range(4):
+    f = h5py.File(dir_+f'long_path{i}.hdf5', 'r')
+    rhos_ex[i] = process_trajectory(f['rho'][:][:M])
+    f.close()
+Uex = rho2U(rhos_ex)
+
+eps = 0.0
+delta = -1.0
+H = eps*sz + delta*sx
+Ls = H2L(H)
+
+rho0 = np.array([1,0,0,0])
+def plot(U,dt,ax,ls,c,l,start=1):
+    rho = np.array([np.dot(Ui,rho0) for Ui in U])
+    itv = int(dt/0.0005+1e-6)
+    drho = np.array([np.linalg.norm(rhoi-rhoi_ref.flatten()) for rhoi,rhoi_ref in zip(rho,rho_ref[::itv])])
+    x = np.arange(drho.shape[0])*dt
+    ax.plot(x[start:],np.log10(drho[start:]),linestyle=ls,color=c,label=l)
+
+fig1,ax1 = plt.subplots(nrows=1,ncols=1)
+fig2,ax2 = plt.subplots(nrows=1,ncols=1)
+fig3,ax3 = plt.subplots(nrows=1,ncols=1)
+fig4,ax4 = plt.subplots(nrows=1,ncols=1)
+colors = 'blue','orange','green','red','cyan','pink'
+tMs = 1.2,2.4
+def TTMT1(kappa,dt):
+    T = -kappa[::itv][:M-1]
+    T = np.concatenate([np.zeros((1,4,4)),T.copy()],axis=0)
+    print('len(T)=',len(T))
+    T[1] -= np.dot(Ls,Ls)
+    T[1] /= 2
+    T *= dt**2
+    T[1] += np.eye(4)-1j*dt*Ls
+    return T
+for dt,color in zip([0.005,.01,0.02,.05,.1,.2],colors):
+    print('dt=',dt)
+    stop = int(10/dt+1e-6)+1
+    lab = r'$\Omega\Delta t=$'+str(dt)
+    for tM,ls,l in zip(tMs,('-',':'),(lab,None)):
+        # TTM
+        print('tM=',tM)
+        itv = int(dt/0.0005+1e-6)
+        M = int(tM/dt+1e-6)+1
+        U = Uex[::itv][:M]
+        T,_ = U2T(U)
+        print('len(T)=',len(T))
+        U = T2U(U,T,stop)
+        plot(U,dt,ax1,ls,color,l,start=M)
+
+        # TTM from kernel
+        T = TTMT1(kappa,dt)
+        U = T2U(U[:1],T,stop,mitigate=True)
+        plot(U,dt,ax2,ls,color,l)
+
+        # TTM from kernel
+        T = TTMT1(kappa,dt)
+        T[1] += dt**3/6*dddU0
+        T[2:] += dt**3/2*F[::itv][1:M-1]
+        U = T2U(U[:1],T,stop,mitigate=True)
+        plot(U,dt,ax3,ls,color,l)
+
+        # midpoint from kernel
+        G1 = scipy.linalg.expm(-1j*dt*Ls)
+        Gh = scipy.linalg.expm(-1j*dt*Ls/2)
+        itv = int(dt/2/.0005+1e-6)
+        K = kappa[::itv]
+        T = np.zeros((M,4,4),dtype=complex)
+        T[1] = G1-dt**2*np.dot(Gh,K[1])
+        for N in range(2,M):
+            T[N] = -dt**2*np.dot(Gh,K[N*2-1])
+        U = T2U_MPDI(U[:1],T,G1,stop,mitigate=True)
+        plot(U,dt,ax4,ls,color,l)
+
+for ax,fig in zip((ax1,ax2,ax3,ax4),(fig1,fig2,fig3,fig4)):
+    ax.set_xlabel(r'$\Omega t$')
+    ax.set_ylabel(r'$\log_{10}(||\Delta\rho||)$')
+    #ax.set_ylim(-.05,1.05)
+    ax.set_xlim(0,10)
+    ax.legend()
+    fig.subplots_adjust(left=0.16, bottom=0.15, right=0.97, top=0.98)
+fig1.savefig(f'rho_TTM.png', dpi=250)
+fig2.savefig(f'rho_TTM_K.png', dpi=250)
+fig3.savefig(f'rho_TTM_K2.png', dpi=250)
+fig4.savefig(f'rho_MPDI.png', dpi=250)
